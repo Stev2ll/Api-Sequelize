2ad7050b6b1eaf4ddc12454675b4246387232e4a
Stev2ll <llullunab@gmail.com>
2023-07-04 22:51:59 -0500

    new

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Configuración de CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
